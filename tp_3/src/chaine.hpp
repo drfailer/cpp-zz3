4b50abc5f2993c04d312541f3a32e23f5ada9808
drfailer <drfailer.42@gmail.com>
2023-10-25 11:05:01 +0200

    ajout de commentaire et de solutions supplémentaires dans le tp3

--- tp_3/src/chaine.hpp
+++ tp_3/src/chaine.hpp
@@ -12,7 +12,7 @@
 
 /* déclaration des templates du plus général au plus spécifique. */
 template<typename T> std::string chaine(T e);
-template<typename ...T> std::string chaine(T&&... args);
+template<typename ...T> std::string chaine(T... args);
 template<typename ...Args> std::string chaine(const std::tuple<Args...> &t);
 template<typename T, size_t... Is> std::string chaine(const T& t, std::index_sequence<Is...>);
 
@@ -53,11 +53,20 @@ std::string chaine(T e) {
 }
 
 /******************************************************************************/
-/*                              multi arguements                              */
+/*                              chain variadique                              */
 /******************************************************************************/
 
+/* #define LAMBDA_VERSION */
+/* #define STREAM_VERSION */
+#if defined(LAMBDA_VERSION)
+/*
+ * Version où l'on utilise un lambda pour itérer sur les paramères. La syntaxe
+ * est la suivante:
+ *
+ * ([&] { utilisation de args comme paramètre courant }(), ... )
+ */
 template<typename ...T>
-std::string chaine(T&&... args) {
+std::string chaine(T... args) {
     std::ostringstream oss;
 
     /* on utilise un lambda pour itérer sur les arguements.
@@ -67,6 +76,29 @@ std::string chaine(T&&... args) {
      }(), ...);
     return oss.str();
 }
+#elif defined(STREAM_VERSION)
+/*
+ * Version où l'on utilise un ostringstream pour construire la chaine.
+ *
+ * (stream << ... << (opération à réaliser sur args))
+ */
+template<typename ...T>
+std::string chaine(T... args) {
+    std::ostringstream oss;
+    (oss << ... << (chaine(args) + " "));
+    return oss.str();
+}
+#else
+/*
+ * Version où l'on utilise l'opérateur +
+ *
+ * ((opération à réaliser sur args) + ...)
+ */
+template<typename ...T>
+std::string chaine(T... args) {
+    return ((chaine(args) + " ") + ...);
+}
+#endif
 
 /******************************************************************************/
 /*                                   tuples                                   */
